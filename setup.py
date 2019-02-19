1b88e6a4143642a6883d23807b3c08d5a96f586e
smartpatrol <charlesf.0@gmail.com>
2019-02-19 13:57:26 -0500

    new params

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='gamesbiz',
     version='0.0.1rc0',
-
     # Package data
     packages=find_packages(),
     include_package_data=True,
